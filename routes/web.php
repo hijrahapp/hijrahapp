ab47570b5e17f7679fa08a4038ee3e6f840ee451
Omar Adel <omar.adell96@gmail.com>
2025-07-15 11:08:10 +0300

    Add Base Files For Metronic Templase

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Demo1\Index as Demo1Index;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('demo1.index');
 });
+
+// Demo1 routes
+Route::get('/demo1', Demo1Index::class)->name('demo1.index');
