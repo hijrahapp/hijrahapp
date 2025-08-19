60888e1051342042f575300f3545a68aa65cef46
Omar Adel <omar.adell96@gmail.com>
2025-08-19 12:36:47 +0300

    Fix bugs

--- app/Resources/UserAnswerGroupedResource.php
+++ app/Resources/UserAnswerGroupedResource.php
@@ -14,25 +14,27 @@ class UserAnswerGroupedResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $data = [
-//            'user_id' => $this->first()->user_id,
-//            'context_type' => $this->first()->context_type,
-//            'context_id' => $this->first()->context_id,
-            'question' => new QuestionResource($this->first()->question),
-            'userAnswers' => $this->map(function ($userAnswer) {
-                return [
-                    'id' => $userAnswer->answer->id,
-                    'title' => $userAnswer->answer->title,
-                ];
-            }),
-            'created_at' => $this->first()->created_at,
-            'updated_at' => $this->first()->updated_at,
-        ];
-
+        $data = [];
         if ($this->first()->module_id) {
+            $data['question_id'] = $this->first()->question->id;
             $data['module_id'] = $this->first()->module_id;
             $data['module_name'] = $this->first()->module_name;
+
+            $this->first()->question->setAttribute('module_id', $this->first()->module_id);
+            $this->first()->question->setAttribute('module_name', $this->first()->module_name);
+            $this->first()->question->setAttribute('questionId_moduleId', $this->first()->question->id . '_' . $this->first()->module_id);
         }
+
+        $data['question'] = new QuestionResource($this->first()->question);
+
+        $data['userAnswers'] = $this->map(function ($userAnswer) {
+                    return [
+                        'id' => $userAnswer->answer->id,
+                        'title' => $userAnswer->answer->title,
+                    ];
+                });
+        $data['created_at'] = $this->first()->created_at;
+        $data['updated_at'] = $this->first()->updated_at;
 
         return $data;
     }
